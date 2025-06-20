553eb24fb9e6488367c990184f1974a881fad697
Hikaru Tomizawa <tomizawa.licht@gmail.com>
2025-06-20 10:21:24 +0900

    imple: download articles scripts

--- scripts/parse_wiki.py
+++ scripts/parse_wiki.py
@@ -3,10 +3,9 @@ Parse Wikipedia articles .bz2 data.
 """
 
 import bz2
-from logging import INFO, Formatter, StreamHandler, getLogger
 import os
 import xml.etree.ElementTree as ET
-
+from logging import INFO, Formatter, StreamHandler, getLogger
 
 # # ========== Logging Config ==========
 FORMAT = "%(levelname)-8s %(asctime)s - [%(filename)s:%(lineno)d]\t%(message)s"
@@ -36,32 +35,32 @@ def parse_and_save():
     Parse huge Wikipedia article dump in chunks and save to DB.
     """
     logger.info(f"Parsing Wikipedia data from {FILE_PATH}")
-    
+
     # Unzip .bz2 file in stream
     with bz2.open(FILE_PATH, "rt", encoding="utf-8") as f:
         # `interparse` load XML data in chunks and return elememnt when an event occurs
         # "end" event occurs when </page> tag is reached
-        context = ET.interparse(f, events=('end',))
-        
+        context = ET.interparse(f, events=("end",))
+
         for event, elem in context:
             # Head of tag contains namespace
             tag = elem.tag.replace(XML_NAMESPACE, "")
-            
+
             if tag == "page":
                 # Extract title
                 title_elem = elem.find(f"./{XML_NAMESPACE}title")
                 # Return revision/text element
                 text_elem = elem.find(f"./{XML_NAMESPACE}revision/{XML_NAMESPACE}text")
-                
+
                 if title_elem is not None and text_elem is not None:
                     title = title_elem.text
-                    text = text_elem.text
+                    # text = text_elem.text
                     logger.info(f"Article found: {title}")
-                    
-                elem.clear() # IMPOPORTANT: Free memory
-                
+
+                elem.clear()  # IMPOPORTANT: Free memory
+
     logger.info("Parsing complete.")
-    
+
 
 if __name__ == "__main__":
     parse_and_save()
